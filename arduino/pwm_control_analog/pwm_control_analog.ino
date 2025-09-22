e7f4ef3f6e82653e8a08ec3cd03d92db8bb2ce44
Nick Korotysh <nick.korotysh@gmail.com>
2025-09-22 14:47:56 +0200

    pwm_control_analog

--- arduino/pwm_control_analog/pwm_control_analog.ino
+++ arduino/pwm_control_analog/pwm_control_analog.ino
@@ -9,14 +9,18 @@ constexpr uint32_t max_duty = (1 << PWM_BITS) - 1;
 uint32_t last_duty = 0;
 
 void setup() {
+  Serial.begin(115200);
+  while (!Serial) {}
   ledcAttach(LED_GPIO, PWM_FREQ, PWM_BITS);
   analogReadResolution(PWM_BITS);
 }
 
 void loop() {
   uint32_t curr_duty = analogRead(ADC_GPIO);
-  if (curr_duty != last_duty) {
+  if (max(curr_duty, last_duty) - min(curr_duty, last_duty) > 2) {
+    Serial.println(analogReadMilliVolts(ADC_GPIO));
     last_duty = curr_duty;
     ledcWrite(LED_GPIO, curr_duty);
   }
+  delay(10);
 }
